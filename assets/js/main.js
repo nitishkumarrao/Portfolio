1eba133605a86746ba487ca08b8e6a7d4c163945
nknitishkumarrao <boddunitishkumar@gmail.com>
2021-06-26 10:22:25 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,30 +66,18 @@ sr.reveal('.contact__input',{interval: 200});
 
 /**************************** */
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+var slideIndex = 0;
+showSlides();
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].className = slides[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
-
