3b22b532d0a2c731b5c8feb730311a16848482b1
nitishkumarrao <boddunitishkumar@gmail.com>
2021-07-04 00:58:35 +0530

    work updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,3 +99,12 @@ window.onscroll = function() {
 
 }
 
+
+var e = document.getElementById('icon');
+e.onmouseover = function() {
+  document.getElementById('info').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('info').style.display = 'none';
+}
+
